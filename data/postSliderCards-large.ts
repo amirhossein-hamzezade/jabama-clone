c71261d2638589e4cc0bb754a0e55893427559ee
Temporary Name <temp@example.com>
2026-08-19 05:05:17 +0330

    Move homepage mock data to global data folder

---
+++ data/postSliderCards-large.ts
@@ -0,0 +1,49 @@
+import { CardProps } from "@/app/_home-components/Card";
+
+export const postSliderCardsLarge: CardProps[] = [
+  {
+    src: "/images/city/2daefeaf-eb88-4354-a716-ee65adf2c6da.jpg",
+    title: "ویلا دوخوابه پچوک",
+    path: "",
+    province: "مازندران",
+    city: "سوادکوه",
+    variant: "large",
+    star: 4.8,
+  },
+  {
+    src: "/images/city/2daefeaf-eb88-4354-a716-ee65adf2c6da.jpg",
+    title: "ویلا دوخوابه پچوک",
+    path: "",
+    province: "مازندران",
+    city: "سوادکوه",
+    variant: "large",
+    star: 4.8,
+  },
+  {
+    src: "/images/city/2daefeaf-eb88-4354-a716-ee65adf2c6da.jpg",
+    title: "ویلا دوخوابه پچوک",
+    path: "",
+    province: "مازندران",
+    city: "سوادکوه",
+    variant: "large",
+    star: 4.8,
+  },
+  {
+    src: "/images/city/2daefeaf-eb88-4354-a716-ee65adf2c6da.jpg",
+    title: "ویلا دوخوابه پچوک",
+    path: "",
+    province: "مازندران",
+    city: "سوادکوه",
+    variant: "large",
+    star: 4.8,
+  },
+  {
+    src: "/images/city/2daefeaf-eb88-4354-a716-ee65adf2c6da.jpg",
+    title: "ویلا دوخوابه پچوک",
+    path: "",
+    province: "مازندران",
+    city: "سوادکوه",
+    variant: "large",
+    star: 4.8,
+  },
+];
---
+++ data/postSliderCards-small.ts
@@ -0,0 +1,81 @@
+import { CardProps } from "@/app/_home-components/Card";
+
+export const postSliderCardsSmall: CardProps[] = [
+  {
+    src: "/images/city/5dd9b930-6fac-40d2-b0d0-25c1225f0b37.jpg",
+    path: "",
+    province: "مازندران",
+    city: "رامسر",
+    variant: "small",
+  },
+  {
+    src: "/images/city/d3ae6348-8508-45f3-b2a5-7faa1301375a.jpg",
+    path: "",
+    province: "البرز",
+    city: "کردان",
+    variant: "small",
+  },
+  {
+    src: "/images/city/47614494-282e-4e68-b5f3-ebf2c951622b.jpg",
+    path: "",
+    province: "تهران",
+    city: "تهران",
+    variant: "small",
+  },
+  {
+    src: "/images/city/8c957f43-be03-45c5-bfac-d416944b6b13.jpg",
+    path: "",
+    province: "مازندران",
+    city: "نوشهر",
+    variant: "small",
+  },
+  {
+    src: "/images/city/3db69d40-bfbc-4957-a00f-275832c09c83.jpg",
+    path: "",
+    province: "مازندران",
+    city: "تنکابن",
+    variant: "small",
+  },
+  {
+    src: "/images/city/d00bfe3f-2ef2-410d-8c6c-18975ae38b7c.jpg",
+    path: "",
+    province: "مازندران",
+    city: "چالوس",
+    variant: "small",
+  },
+  {
+    src: "/images/city/3c90c4d6-13ce-45b3-b03f-b23ba42859aa.jpg",
+    path: "",
+    province: "مازندران",
+    city: "شیرگاه",
+    variant: "small",
+  },
+  {
+    src: "/images/city/2daefeaf-eb88-4354-a716-ee65adf2c6da.jpg",
+    path: "",
+    province: "گیلان",
+    city: "رشت",
+    variant: "small",
+  },
+  {
+    src: "/images/city/12c7a1f8-de13-4e8d-bc58-f80e655b7da7.jpg",
+    path: "",
+    province: "مازندران",
+    city: "سوادکوه",
+    variant: "small",
+  },
+  {
+    src: "/images/city/ae6db662-97fb-4b5f-bc4c-6c724ffe32fa.jpg",
+    path: "",
+    province: "گیلان",
+    city: "تالش",
+    variant: "small",
+  },
+  {
+    src: "/images/city/a84cd23d-ea99-4457-bbff-adff03f28ecd.jpg",
+    path: "",
+    province: "گیلان",
+    city: "بندرانزلی",
+    variant: "small",
+  },
+];
